8c9ffe61362de6fe6cca2ccf5e5349beba6f5fbd
Mark Pinnock <m.pinnock@instadeep.com>
2024-04-10 08:48:19 +0100

    Add normlisation enum

--- src/generative/common/constants.py
+++ src/generative/common/constants.py
@@ -5,7 +5,15 @@ import enum
 
 @enum.unique
 class Datasets(str, enum.Enum):
-    """Datasets enum."""
+    """Dataset types."""
 
     CIFAR10 = "cifar10"
     MNIST = "mnist"
+
+
+@enum.unique
+class Normalisation(str, enum.Enum):
+    """Normalisation types."""
+
+    _01 = "01"  # Normalise to range [0, 1]
+    _11 = "-11"  # Normalise to range [-1, 1]
