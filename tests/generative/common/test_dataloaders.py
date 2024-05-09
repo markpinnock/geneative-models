87762c4dc4da09e4e6db111d2e3d2040e8964e8c
Mark Pinnock <m.pinnock@instadeep.com>
2024-05-09 19:17:17 +0100

    Add resize dataset test

--- tests/generative/common/test_dataloaders.py
+++ tests/generative/common/test_dataloaders.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import tensorflow as tf
 from generative.common.constants import Normalisation
 from generative.common.dataloaders import add_channel_dim, normalise, resize_dataset
 
@@ -44,5 +45,18 @@ def test_normalise(normalisation: str, expected_min_max: list[float]) -> None:
     assert dataset.max() == expected_min_max[1]
 
 
-def test_resize_dataset() -> None:
+@pytest.mark.parametrize(
+    "data_dims,expected_dims",
+    [
+        ([2, 4, 4, 1], [2, 8, 8, 1]),
+        ([2, 4, 4, 1], [2, 16, 16, 1]),
+        ([2, 4, 4, 3], [2, 8, 8, 3]),
+        ([2, 4, 4, 3], [2, 16, 16, 3]),
+    ],
+)
+def test_resize_dataset(data_dims: list[int], expected_dims: list[int]) -> None:
     """Test resizing dataset."""
+    dataset = tf.zeros(data_dims)
+    dataset = resize_dataset(expected_dims[1:3], dataset)
+
+    assert list(dataset.shape) == expected_dims
