cd7fb3b9d06c8be21fbcb942f69941a5e03330f9
Mark Pinnock <m.pinnock@instadeep.com>
2024-05-11 19:06:26 +0100

    Add test dataset fixture

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+"""Configuration for pytest."""
+
+import numpy as np
+from pathlib import Path
+from typing import Any
+import pytest
+
+TEST_IMG_DIMS_3D = [8, 4, 4]
+TEST_IMG_DIMS_4D = [8, 4, 4, 3]
+
+
+@pytest.fixture(scope="session", params=[TEST_IMG_DIMS_3D, TEST_IMG_DIMS_4D])
+def create_test_dataset(tmp_path_factory: pytest.TempPathFactory, request: Any) -> Path:
+    """Create a test dataset for both black and white and RGB images."""
+    tmp_dir = tmp_path_factory.mktemp("dataset")
+    save_dir = tmp_dir / "dataset.npz"
+    dataset = np.zeros(request.param)
+    dataset[:, 0, 0] = 255
+    np.savez(save_dir, x_train=dataset)
+
+    return save_dir
