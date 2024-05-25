3dd6cab30141e338ebbe162cc58544e6d8713d4e
Mark Pinnock <m.pinnock@instadeep.com>
2024-05-25 14:17:45 +0100

    Add discriminator

--- src/generative/gans/dc_gan.py
+++ src/generative/gans/dc_gan.py
@@ -1,4 +1,6 @@
+import numpy as np
 import tensorflow as tf
+from omegaconf import DictConfig
 
 from generative.common.activations import Activation
 
@@ -151,3 +153,90 @@ class UpBlock(tf.keras.layers.Layer):
             x = self.bn(x)
 
         return self.activation(x)
+
+
+class Discriminator(tf.keras.Model):
+    """Discriminator for DCGAN.
+
+    Args:
+        cfg: config
+        name: model name
+    """
+
+    def __init__(self, cfg: DictConfig, name: str = "discriminator"):
+        super().__init__(name=name)
+        initialiser = tf.keras.initializers.RandomNormal(0, 0.02)
+        self.blocks = []
+
+        # Get start and end resolutions
+        assert cfg["img_dims"][0] == cfg["img_dims"][1], "Only square images supported"
+        self._start_resolution = cfg["img_dims"][0]
+        self._end_resolution = 4
+        self._img_dims = cfg["img_dims"]  # Needed for summary method
+
+        # Get number of layers and channels
+        self._num_layers = int(np.log2(self._start_resolution)) - int(
+            np.log2(self._end_resolution),
+        )
+        self._channels = [
+            np.min(
+                [(cfg.discriminator_channels * 2**i), cfg.max_channels],
+            )
+            for i in range(self.num_layers)
+        ]
+
+        # Create layers
+        for i in range(self.num_layers):
+            self.blocks.append(
+                DownBlock(
+                    channels=self._channels[i],
+                    initialiser=initialiser,
+                    activation=cfg.discriminator_activation,
+                    batchnorm=True,
+                    final=False,
+                    name=f"dn{i}",
+                ),
+            )
+
+        # Add final conv layer or dense block as needed
+        if cfg.discriminator_dense:
+            self.blocks.append(tf.keras.layers.Flatten())
+            self.blocks.append(
+                DCDense(
+                    units=1,
+                    activation=cfg.discriminator_activation,
+                    kernel_initializer=initialiser,
+                    name="dense",
+                ),
+            )
+
+        else:
+            self.blocks.append(
+                DownBlock(
+                    channels=1,
+                    initialiser=initialiser,
+                    activation="linear",
+                    final=True,
+                    name="final",
+                ),
+            )
+
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        """Forward pass through discriminator."""
+        for block in self.blocks:
+            x = block(x)
+
+        return x
+
+    @property
+    def num_layers(self) -> int:
+        return self._num_layers
+
+    @property
+    def channels(self) -> list[int]:
+        return self._channels
+
+    def summary(self) -> None:
+        """Print model summary."""
+        x = tf.keras.layers.Input(self._img_dims)
+        tf.keras.Model(inputs=[x], outputs=self.call(x), name="Discriminator").summary()
