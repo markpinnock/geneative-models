0b86c8711d12880b956f4d6a3b85009130fd6900
Mark Pinnock <m.pinnock@instadeep.com>
2024-06-12 07:37:20 +0100

    Remove loss factory

--- src/generative/common/losses.py
+++ src/generative/common/losses.py
@@ -5,6 +5,8 @@ from typing import Callable
 
 import tensorflow as tf
 
+from generative.common.registry import Registry, Categories
+
 
 @enum.unique
 class LossesEnum(str, enum.Enum):
@@ -15,33 +17,3 @@ class LossesEnum(str, enum.Enum):
     def __str__(self) -> str:
         """Get string representation."""
         return self.value
-
-
-class LossMeta(type):
-    """Metaclass for loss function factory."""
-
-    _losses: dict[str, type[tf.keras.losses.Loss]]
-
-    def __contains__(cls, x: str) -> bool:
-        """Check if loss function in dictionary.
-
-        Args:
-            x: loss function to check for
-        """
-        return x in cls._losses
-
-    def __getitem__(cls, x: str) -> type[tf.keras.losses.Loss]:
-        """Return loss function from dictionary.
-
-        Args:
-            x: loss function to return
-        """
-        return cls._losses[x]
-
-
-class Loss(metaclass=LossMeta):
-    """Activation function factory."""
-
-    _losses = {
-        LossesEnum.BINARY_CROSSENTROPY: tf.keras.losses.BinaryCrossentropy,
-    }
