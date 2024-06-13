3e6ba824b0b4adff5403ecb9c9733eb4be6978e1
Mark Pinnock <m.pinnock@instadeep.com>
2024-06-13 20:06:43 +0100

    Add tests for folder-based dataset

--- tests/generative/common/test_dataloaders.py
+++ tests/generative/common/test_dataloaders.py
@@ -8,7 +8,13 @@ from omegaconf import DictConfig
 import tensorflow as tf
 
 from generative.common.constants import Normalisation, DataSplits
-from generative.common.dataloaders import add_channel_dim, normalise, resize_dataset, get_dataset_from_file
+from generative.common.dataloaders import (
+    get_dataset_from_file,
+    get_dataset_from_folder,
+    add_channel_dim,
+    normalise,
+    resize_dataset,
+)
 
 
 @pytest.mark.parametrize(
@@ -88,7 +94,7 @@ def test_resize_dataset_fail(expected_dims: list[int]) -> None:
     ],
 )
 def test_get_dataset_from_file(
-    create_test_dataset: Path,
+    create_test_dataset_file: Path,
     img_dims: list[int],
     normalisation: str,
     batch_size: int,
@@ -98,12 +104,53 @@ def test_get_dataset_from_file(
         {
             "img_dims": img_dims,
             "normalisation": normalisation,
-            "data_dir": create_test_dataset.parent,
+            "data_dir": create_test_dataset_file.parent,
             "dataset_name": "dataset",
             "batch_size": batch_size,
+            "n_critic": 1,
         },
     )
     dataset = get_dataset_from_file(cfg, DataSplits.TRAIN)
+    img_batch = next(iter(dataset))
+
+    assert img_batch.ndim == 4
+    assert img_batch.shape[0] == batch_size
+    assert list(img_batch.shape)[1:3] == img_dims
+
+    if normalisation == Normalisation.NEG_ONE_ONE:
+        assert tf.reduce_min(img_batch) == -1.0
+    else:
+        assert tf.reduce_min(img_batch) == 0.0
+    assert tf.reduce_max(img_batch) == 1.0
+
+
+@pytest.mark.parametrize(
+    "img_dims,normalisation,batch_size",
+    [
+        ([4, 4], Normalisation.ZERO_ONE, 2),
+        ([4, 4], Normalisation.NEG_ONE_ONE, 4),
+        ([8, 8], Normalisation.ZERO_ONE, 2),
+        ([8, 8], Normalisation.NEG_ONE_ONE, 4),
+    ],
+)
+def test_get_dataset_from_folder(
+    create_test_dataset_folder: Path,
+    img_dims: list[int],
+    normalisation: str,
+    batch_size: int,
+) -> None:
+    """Test loading dataset from file."""
+    cfg = DictConfig(
+        {
+            "img_dims": img_dims,
+            "normalisation": normalisation,
+            "data_dir": create_test_dataset_folder,
+            "dataset_name": "dataset",
+            "batch_size": batch_size,
+            "n_critic": 1,
+        },
+    )
+    dataset = get_dataset_from_folder(cfg)
     img_batch = next(iter(dataset))
 
     assert img_batch.ndim == 4
