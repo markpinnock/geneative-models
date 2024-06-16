33a46b369b356e8f4997751b2e861bd3fdd1f307
Mark Pinnock <m.pinnock@instadeep.com>
2024-06-16 17:56:04 +0100

    Add WGAN-GP unit tests

--- tests/generative/gans/test_wasserstein.py
+++ tests/generative/gans/test_wasserstein.py
@@ -51,15 +51,16 @@ def test_no_wasserstein(dense: bool) -> None:
     model = DCGAN(cfg)
     assert not hasattr(model, "n_critic")
 
-    # Build and check no weight clipping constraints in variables
+    # Build and check correctly initialised
     build_layer(img_dims, model.discriminator)
     build_layer([latent_dim], model.generator)
     assert len(model.variables) > 0
 
-    for var in model.discriminator.variables:
-        assert var.constraint is None
-    for var in model.generator.variables:
-        assert var.constraint is None
+    # Check no weight clipping constraints in any variables
+    for variable in model.discriminator.variables:
+        assert variable.constraint is None
+    for variable in model.generator.variables:
+        assert variable.constraint is None
 
 
 @pytest.mark.parametrize("dense", [False, True])
@@ -92,21 +93,25 @@ def test_orig_wasserstein(dense: bool) -> None:
             "clip_value": 0.01,
         },
     )
-    # Initialise model and check no n_critic attribute
+    # Initialise model and check has n_critic attribute
     model = DCGAN(cfg)
     assert hasattr(model, "n_critic")
 
-    # Build and check weight clipping constraints in correct variables
+    # Build and check correctly initialised without gradient penalty
     build_layer(img_dims, model.discriminator)
     build_layer([latent_dim], model.generator)
     assert len(model.variables) > 0
+    assert not model.gradient_penalty
 
+    # Check weight clipping constraints in discriminator variables
     for variable in model.discriminator.variables:
         if variable.ndim > 1:
             assert isinstance(variable.constraint, WeightClipConstraint)
 
-    for var in model.generator.variables:
-        assert var.constraint is None
+    # Check not weight clipping constraints in generator variables
+    for variable in model.generator.variables:
+        assert variable.constraint is None
+
 
 def test_orig_wasserstein_fail() -> None:
     """Test Wasserstein initialisation fails without arguments."""
@@ -131,8 +136,91 @@ def test_orig_wasserstein_fail() -> None:
                 "dense": False,
                 "output": "linear",
             },
+            "wasserstein_type": WassersteinTypes.CLIP_WEIGHTS,
+            "n_critic": 5,
         },
     )
-    # Initialise model and check no n_critic attribute
+    # Initialise model and check fails
+    with pytest.raises(ConfigAttributeError):
+        _ = DCGAN(cfg)
+
+
+@pytest.mark.parametrize("dense", [False, True])
+def test_wasserstein_gp(dense: bool) -> None:
+    """Test original Wasserstein initialisation with weight clipping."""
+    img_dims = [32, 32, 3]
+    latent_dim = 4
+    cfg = DictConfig(
+        {
+            "batch_size": 2,
+            "img_dims": img_dims,
+            "latent_dim": latent_dim,
+            "loss": LossTypes.WASSERSTEIN,
+            "num_examples": 4,
+            "model_name": "dcgan",
+            "max_channels": 4,
+            "discriminator": {
+                "activation": "leaky_relu",
+                "channels": 1,
+                "dense": dense,
+            },
+            "generator": {
+                "activation": "relu",
+                "channels": 1,
+                "dense": dense,
+                "output": "linear",
+            },
+            "wasserstein_type": WassersteinTypes.GRADIENT_PENALTY,
+            "n_critic": 5,
+            "gradient_penalty": 10,
+        },
+    )
+    # Initialise model and check has n_critic attribute
+    model = DCGAN(cfg)
+    assert hasattr(model, "n_critic")
+
+
+    # Build and check correctly initialised without gradient penalty
+    build_layer(img_dims, model.discriminator)
+    build_layer([latent_dim], model.generator)
+    assert len(model.variables) > 0
+    assert model.gradient_penalty
+
+    # Check no weight clipping constraints in any variables
+    for variable in model.discriminator.variables:
+        assert variable.constraint is None
+
+    for variable in model.generator.variables:
+        assert variable.constraint is None
+
+
+def test_wasserstein_gp_fail() -> None:
+    """Test Wasserstein initialisation fails without arguments."""
+    img_dims = [32, 32, 3]
+    cfg = DictConfig(
+        {
+            "batch_size": 2,
+            "img_dims": img_dims,
+            "latent_dim": 4,
+            "loss": LossTypes.WASSERSTEIN,
+            "num_examples": 4,
+            "model_name": "dcgan",
+            "max_channels": 4,
+            "discriminator": {
+                "activation": "leaky_relu",
+                "channels": 1,
+                "dense": False,
+            },
+            "generator": {
+                "activation": "relu",
+                "channels": 1,
+                "dense": False,
+                "output": "linear",
+            },
+            "wasserstein_type": WassersteinTypes.GRADIENT_PENALTY,
+            "n_critic": 5,
+        },
+    )
+    # Initialise model and check fails
     with pytest.raises(ConfigAttributeError):
         _ = DCGAN(cfg)
